f07b85c746e703dbac3ec77824023b128502f7a8
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-03-29 11:51:38 -0600

    The plants are alive

---
+++ plants.js
@@ -0,0 +1,11 @@
+var gets_sunshine = true;
+var ounces_of_water = 15;
+
+if (gets_sunshine === true && ounces_of_water >= 10) {
+    console.log("It's ALIVE!!!!");
+} else {
+    console.log("Death to us all!");
+}
+
+
+//(gets_sunshine === false || ounces_of_water <= 10)
