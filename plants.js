2293ae355effe7eab25fcbf6a1299a4e50c6f7d7
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-03-29 11:54:15 -0600

    add or statement

--- plants.js
+++ plants.js
@@ -1,7 +1,7 @@
-var gets_sunshine = true;
-var ounces_of_water = 15;
+var gets_sunshine = false;
+var ounces_of_water = 6;
 
-if (gets_sunshine === true && ounces_of_water >= 10) {
+if (gets_sunshine === true || ounces_of_water >= 10) {
     console.log("It's ALIVE!!!!");
 } else {
     console.log("Death to us all!");
